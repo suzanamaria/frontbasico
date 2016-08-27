00a7f289d5ddbf00a3eef171b6bee5da28932e5f
suzanamaria[user] <smariabs20@gmail.com>
2016-08-27 16:52:04 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function validaInfo() {
+    if (document.formulario.login.value == "") {
+       document.formulario.login.focus();
+       document.formulario.login.style.borderColor="#ff0000";
+       document.getElementById("msg_erro").style.visibility = "visible";      
+       return false;
+    }else{
+        document.formulario.login.style.borderColor="transparent";
+        document.getElementById("msg_erro").style.visibility = "hidden"; 
+    }
+    
+    if (document.formulario.senha.value == ""){
+        document.formulario.senha.focus();
+        document.formulario.senha.style.borderColor="#ff0000";
+        document.getElementById("msg_erro").style.visibility = "visible"; 
+        return false;
+    }else{
+        document.formulario.login.style.borderColor="transparent";
+    }
+}
